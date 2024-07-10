ece6332e630a182238fae407fc2086877fd2bafa
sharmachait <chait8126@gmail.com>
2024-07-11 00:16:48 +0530

    ping

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace codecrafters_redis
 {
@@ -16,19 +17,13 @@ namespace codecrafters_redis
         public void Start()
         {
             _server.Start();
-            Console.WriteLine("Server started, waiting for a client to connect...");
-
-            // Wait for a client to connect
             Socket clientSocket = _server.AcceptSocket();
-            Console.WriteLine("Client connected!");
-
-            // Here you can add code to handle the connected client
+            clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"));
         }
 
         public void Stop()
         {
             _server.Stop();
-            Console.WriteLine("Server stopped.");
         }
     }
 
@@ -36,14 +31,8 @@ namespace codecrafters_redis
     {
         static void Main(string[] args)
         {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            Console.WriteLine("Logs from your program will appear here!");
-
             TcpServer server = new TcpServer(IPAddress.Any, 6379);
             server.Start();
-
-            // Optionally stop the server after some time or based on some condition
-            // server.Stop();
         }
     }
 }
