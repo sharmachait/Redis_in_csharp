98cae485321db7d20481fc7c7926c4653effe9cc
sharmachait <chait8126@gmail.com>
2024-07-12 07:10:01 +0530

    maybe

--- src/Server.cs
+++ src/Server.cs
@@ -7,6 +7,8 @@ namespace codecrafters_redis
     class TcpServer
     {
         private readonly TcpListener _server;
+        private Dictionary<int, Socket> connectedSockets = new Dictionary<int, Socket>();
+        private int c = 0;
         public TcpServer(IPAddress ipAddress, int port)
         {
             _server = new TcpListener(ipAddress, port);
@@ -15,22 +17,27 @@ namespace codecrafters_redis
         public async Task StartAsync()
         {
             _server.Start();
+            Console.WriteLine("Server started...");
 
             while (true)
             {
                 Socket clientSocket = await _server.AcceptSocketAsync();
-                await HandleSocketAsync(clientSocket);
+                connectedSockets.Add(c++,clientSocket);
+                 ClientHandler(c);
             }
         }
 
-        async Task HandleSocketAsync(Socket clientSocket)
+        public async Task ClientHandler(int key)
         {
+            Socket clientSocket = connectedSockets[key];
             while (clientSocket.Connected)
             {
-                var command = new byte[clientSocket.ReceiveBufferSize];
-                await clientSocket.ReceiveAsync(command);
-                await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"));
+                byte[] buffer = new byte[1024];
+                await clientSocket.ReceiveAsync(buffer);
+                await clientSocket.SendAsync(Encoding.ASCII.GetBytes("+PONG\r\n"));
             }
+            clientSocket.Dispose();
+            connectedSockets.Remove(key);
         }
     }
 
