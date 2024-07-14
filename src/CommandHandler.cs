d877358c6b0bd9d3aabd558f7e49d28d03aa2b75
sharmachait <chait8126@gmail.com>
2024-07-14 19:41:28 +0530

    expiry

--- src/CommandHandler.cs
+++ src/CommandHandler.cs
@@ -15,17 +15,44 @@ public class CommandHandler
                 _response = $"+{command[1]}\r\n";
                 break;
             case "get":
+                DateTime currTime = DateTime.Now;
                 try
                 {
-                    _response = $"+{store.GetMap()[command[1]]}\r\n";
+                    Value val = store.GetMap()[command[1]];
+                    if(currTime >= val.expiry)
+                    {
+                        _response = $"+{val.val}\r\n";
+                    }
+                    else
+                    {
+                        store.GetMap().Remove(command[1]);
+                        _response = $"$-1\r\n";
+                    }
                 }
                 catch (KeyNotFoundException)
                 {
+                    
                     _response = "error getting value";
                 }
                 break;
             case "set":
-                store.GetMap()[command[1]] = command[2];
+                DateTime currTime = DateTime.Now;
+                if (command.Length == 3)
+                {
+                    
+                    DateTime expiry = DateTime.MaxValue;
+                    Value val = new Value(command[2], currTime, expiry);
+                    store.GetMap()[command[1]] = val;
+                }
+                else if (command.Length == 5 && command[3].Equals("pc"))
+                {
+                    int delta = int.Parse(command[4]);
+                    
+                    DateTime expiry = currTime.AddMilliseconds(delta);
+                    Value val = new Value(command[2], currTime, expiry);
+                    store.GetMap()[command[1]] = val;
+                } 
+                
                 _response = "+OK\r\n";
                 break;
             default:
--- src/Store.cs
+++ src/Store.cs
@@ -14,3 +14,16 @@ public class Store
         return this.map;
     }
 }
+
+public class Value {
+    public String val;
+    public DateTime created;
+    public DateTime expiry;
+    public Value(string val, DateTime created, DateTime expiry)
+    {
+        this.val = val;
+        this.created = created;
+        this.expiry = expiry;
+    }
+
+}
