5ca162fcac6f672994d85282006a2a110d414702
sharmachait <chait8126@gmail.com>
2024-07-28 00:58:35 +0530

    response

--- src/CommandHandler.cs
+++ src/CommandHandler.cs
@@ -111,5 +111,6 @@ public class CommandHandler
     {
         string replication = "# Replication\nrole:master";
         _response = _parser.MakeBulkString(replication);
+        Console.WriteLine(_response);
     }
 }
