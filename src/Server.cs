00ac11091b58a488d5c2359c263efa8c1d70230a
sharmachait <chait8126@gmail.com>
2024-07-27 23:26:45 +0530

    args

--- src/Server.cs
+++ src/Server.cs
@@ -6,9 +6,14 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        
-        TcpServer server = new TcpServer(IPAddress.Any, 6379);
-        await server.StartAsync(args);
+        if (args.Length > 0 && args[0].Equals("--port")) {
+            TcpServer server = new TcpServer(IPAddress.Any, int.Parse(args[1]));
+            await server.StartAsync(args);
+        }
+        else {
+            TcpServer server = new TcpServer(IPAddress.Any, 6379);
+            await server.StartAsync(args);
+        }
     }
 }
 
