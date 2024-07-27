f347f8c2344f2ee2e0eb7db0edc2b2d1ba2841c3
sharmachait <chait8126@gmail.com>
2024-07-27 23:20:01 +0530

    testing

--- src/Server.cs
+++ src/Server.cs
@@ -6,6 +6,7 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        Console.WriteLine(args.Length);
         TcpServer server = new TcpServer(IPAddress.Any, 6379);
         await server.StartAsync();
     }
