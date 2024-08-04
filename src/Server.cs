7cd8f789d51db4fadb79ad21e4254a1246140299
sharmachait <chait8126@gmail.com>
2024-08-04 12:24:22 +0530

    slavery

--- src/Server.cs
+++ src/Server.cs
@@ -79,25 +79,22 @@ class Program
 
 
 
-
-            string ReplconfCapa = "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
             string[] ReplconfCapaCommand = ["REPLCONF", "capa", "psync2"];
+
             stream.Write(Encoding.UTF8.GetBytes(parser.RespArray(ReplconfCapaCommand)));
 
             response = reader.ReadLine();
-            Console.WriteLine("capa response: "+ response);
 
             if (!"+OK".Equals(response))
                 return null;
 
 
 
+            string[] PsyncCommand = ["PSYNC", "?", "-1"];
 
-            string Psync = "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n";
-            stream.Write(Encoding.UTF8.GetBytes(Psync));
+            stream.Write(Encoding.UTF8.GetBytes(parser.RespArray(PsyncCommand)));
 
             response = reader.ReadLine();
-            Console.WriteLine("psync response: "+response);
 
             if (response == null ||  ! "+FULLRESYNC".Equals(response.Substring(0, response.IndexOf(" "))))
             {
