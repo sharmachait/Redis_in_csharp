f092c7ef53a39d8383f151c4198349f6c562c377
sharmachait <chait8126@gmail.com>
2024-08-25 01:21:00 +0530

    parallel?

--- src/TcpServer.cs
+++ src/TcpServer.cs
@@ -67,11 +67,7 @@ class TcpServer
 
             foreach (string[] command in commands)
             {
-                Console.WriteLine("Command received from client");
-                foreach(string c in command)
-                {
-                    Console.Write(c+" ");
-                }
+
                 string response = await _handler.Handle(command, client);
                 client.Send(response);
             }
@@ -150,11 +146,7 @@ class TcpServer
             
             foreach (string[] command in commands)
             {
-                Console.WriteLine("Command received from master");
-                foreach (string c in command)
-                {
-                    Console.Write(c + " ");
-                }
+                
                 string response = await _handler.HandleMasterCommands(command);
             }
         }
