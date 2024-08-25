8bc1231ead8916fc8908364755c587e0d1a5a986
sharmachait <chait8126@gmail.com>
2024-08-25 21:31:09 +0530

    hsgfgkjfhsdgk

--- src/RespParser.cs
+++ src/RespParser.cs
@@ -12,6 +12,7 @@ public class RespParser
     public List<string[]> Deserialize(byte[] command)
     {
         string _data = Encoding.UTF8.GetString(command);
+        Console.WriteLine(_data);
         _data = _data.Substring(0, _data.IndexOf('\0'));
 
         string[] commands = _data.Split('*');
